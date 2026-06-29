25c4fcdaf7f45138bf9dbdb1dda3788a6712918b
Harshaan Rehal <shaan9080@gmail.com>
2026-06-28 20:15:22 -0400

    Initial Commit

---
+++ NotchIsland/NotchIslandApp.swift
@@ -0,0 +1,17 @@
+//
+//  NotchIslandApp.swift
+//  NotchIsland
+//
+//  Created by Shaan Rehal on 2026-06-28.
+//
+
+import SwiftUI
+
+@main
+struct NotchIslandApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
